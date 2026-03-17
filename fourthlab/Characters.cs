9fd10b9fdd312bc2c40e4eaff45ea21170835a18
Грязнов Алексей Сергеевич <gryznov.gr@gmail.com>
2026-03-17 13:19:20 +0800

    create form view

--- fourthlab/Characters.cs
+++ fourthlab/Characters.cs
@@ -7,20 +7,28 @@ using System.Threading.Tasks;
 // БУДТЕ ВНИМАТЕЛЬНЕЕ: ТУТ ДОЛЖЕН СТОЯТЬ ТОТ ЖЕ namespace что и в Program.cs
 namespace fourthlab
 {
-    public class Warrior
-    {
+    public class Character { }
 
+    public class Warrior : Character
+    {
+        public int health = 200;
+        public int damage = 20;
+        public int armor = 20;
     }
 
-    public class Mage
+    public enum MageType {Fire, Water, Earth, Air}
+    public class Mage : Character
     {
-        public int health = 100;
-        public int damage = 10;
-        public int mana = 10;
+        public int health = 75;
+        public int damage = 15;
+        public int mana = 50;
+        public MageType type = MageType.Air;
     }
 
-    public class Archer
+    public class Archer : Character
     {
-
+        public int health = 75;
+        public int damage = 15;
+        public int arrows = 15;
     }
 }
