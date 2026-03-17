1bf7ef85616dfb6ef7def43017fff59bd7f8a396
gryaznovas <gryznov.gr@gmail.com>
2026-03-17 12:08:21 +0800

    Добавьте файлы проекта.

---
+++ fourthlab/Characters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// БУДТЕ ВНИМАТЕЛЬНЕЕ: ТУТ ДОЛЖЕН СТОЯТЬ ТОТ ЖЕ namespace что и в Program.cs
+namespace fourthlab
+{
+    public class Warrior
+    {
+
+    }
+
+    public class Mage
+    {
+        public int health = 100;
+        public int damage = 10;
+        public int mana = 10;
+    }
+
+    public class Archer
+    {
+
+    }
+}
