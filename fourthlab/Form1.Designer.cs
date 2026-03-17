4492144a31eca57914e06decf2c77283a85116fd
Грязнов Алексей Сергеевич <gryznov.gr@gmail.com>
2026-03-17 13:20:04 +0800

    create form view 2.0

--- fourthlab/Form1.Designer.cs
+++ fourthlab/Form1.Designer.cs
@@ -28,31 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            addButton = new Button();
+            takeButton = new Button();
+            txtOut = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // pictureBox1
             // 
-            button1.Location = new Point(12, 12);
-            button1.Name = "button1";
-            button1.Size = new Size(146, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            pictureBox1.Image = Properties.Resources.Group_10;
+            pictureBox1.Location = new Point(22, 16);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(323, 398);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // addButton
+            // 
+            addButton.Location = new Point(232, 144);
+            addButton.Name = "addButton";
+            addButton.Size = new Size(98, 23);
+            addButton.TabIndex = 1;
+            addButton.Text = "Пополнить";
+            addButton.UseVisualStyleBackColor = true;
+            // 
+            // takeButton
+            // 
+            takeButton.Location = new Point(244, 316);
+            takeButton.Name = "takeButton";
+            takeButton.Size = new Size(75, 46);
+            takeButton.TabIndex = 2;
+            takeButton.Text = "Взять";
+            takeButton.UseVisualStyleBackColor = true;
+            // 
+            // txtOut
+            // 
+            txtOut.Location = new Point(53, 316);
+            txtOut.Name = "txtOut";
+            txtOut.Size = new Size(167, 46);
+            txtOut.TabIndex = 3;
+            txtOut.Text = "";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(413, 342);
-            Controls.Add(button1);
+            ClientSize = new Size(357, 424);
+            Controls.Add(txtOut);
+            Controls.Add(takeButton);
+            Controls.Add(addButton);
+            Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
+        private PictureBox pictureBox1;
+        private Button addButton;
+        private Button takeButton;
+        private RichTextBox txtOut;
     }
 }
